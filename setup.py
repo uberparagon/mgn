bebc795e6778db315f0b7144c07024f8a666722f
Drew Johnson <werd2.718@gmail.com>
2017-09-12 11:57:37 -0700

    maybe some changes

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from distutils.core import setup
 
+try:
+    from sage.env import SAGE_SRC, SAGE_VERSION
+except ImportError:
+    print "WARNING: This package only works with SageMath."
+
+
+with open("README.rst") as f:
+    long_description = f.read()
+    
 setup(
     name='mgn',
-    version='1.0.0',
+    version='1.0.3',
     packages=['strataalgebra', 'topintersections'],
     url='https://github.com/uberparagon/mgn',
+    download_url="https://github.com/uberparagon/mgn/releases/latest/",
     license='TODO',
     author='Drew Johnson',
     author_email='werd2.718@gmail.com',
-    description='A Sage program for computing products and Faber-Zagier-Pixton relations on the moduli space of stable curves.'
+    description='A Sage program for computing products, Faber-Zagier relations, and top intersections on the moduli space of stable curves.',
+    long_description = long_description
 )
